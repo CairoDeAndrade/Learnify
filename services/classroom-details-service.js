5aecaa91161a3bfec4c76c733250a55859860ae5
cairo <cairo.andrade777@gmail.com>
2024-08-25 17:39:34 -0300

    feat(classroom-details): Redirecting to classroom details page by id

---
+++ services/classroom-details-service.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    getStudentUsername();
+    getClassroomById();
+});
+
+const API_URL = "https://65d8cad7c96fbb24c1bc5095.mockapi.io/api/v1/classrooms";
+
+function getClassroomById() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const classroomId = urlParams.get('id');
+
+    if (classroomId) {
+        fetch(`${API_URL}/${classroomId}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar a turma');
+                }
+                return response.json();
+            })
+            .then(data => {
+                renderClassroomDetails(data);
+            })
+            .catch(error => {
+                console.error("Erro:", error);
+                alert("Ocorreu um erro ao carregar os detalhes da turma.");
+            });
+    } else {
+        alert("Nenhuma turma selecionada.");
+    }
+}
+
+function renderClassroomDetails(classroom) {
+    console.log(classroom);
+}
