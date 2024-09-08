e5ae4fc462bb1162c1f5a8c5559ed5fb0161f7af
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2024-09-08 16:23:28 -0300

    Caminhos + Design Neon + Loaders + Idioma PTBR

---
+++ loaders/loader-font.js
@@ -0,0 +1,12 @@
+const fontHTML = ` 
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700&display=swap"
+            rel="stylesheet"
+            />
+`;
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.insertAdjacentHTML("afterbegin", fontHTML);
+});
--- services/login-service.js
+++ services/login-service.js
@@ -1,32 +1,32 @@
 function login(event) {
-    event.preventDefault(); 
-    
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+  event.preventDefault();
 
-    validateUserCredentials(username, password);
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  validateUserCredentials(username, password);
 }
 
 function validateUserCredentials(username, password) {
-    if (username === 'admin' && password === 'admin') {
-        storeLoginData(username);
-        redirectTo('/components/classroom/classroom.html');
-    } else {
-        alert('Login inválido');
-    }
+  if (username === "admin" && password === "admin") {
+    storeLoginData(username);
+    redirectTo("/components/classroom/classroom.html");
+  } else {
+    alert("Login inválido");
+  }
 }
 
 function storeLoginData(user) {
-    sessionStorage.setItem('logged', 'true');
-    sessionStorage.setItem('user', user);
+  sessionStorage.setItem("logged", "true");
+  sessionStorage.setItem("user", user);
 }
 
 function getStudentUsername() {
-    const username = sessionStorage.getItem("user");
-    if (username) {
-        document.getElementById("username").textContent = username;
-    } else {
-        alert("Usuário não logado. Redirecionando para a página de login.");
-        window.location.href = "index.html";
-    }
+  const username = sessionStorage.getItem("user");
+  if (username) {
+    document.getElementById("username").textContent = username;
+  } else {
+    alert("Usuário não logado. Redirecionando para a página de login.");
+    window.location.href = "index.html";
+  }
 }
